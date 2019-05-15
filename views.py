a9eb6a82a37692b4be9e0fba7eb1057826b8318c
Константин <olenov1994@gmail.com>
2019-05-15 17:24:14 +0900

    init

--- views.py
+++ views.py
@@ -62,7 +62,7 @@ def add_student():
     return redirect(url_for('main'))
 
 
-@app.route('/addpage', methods=['POST'])
+@app.route('/addpage')
 def adding():
     return render_template('add.html')
 
@@ -87,44 +87,76 @@ def edit(id):
 
 @app.route('/editing',methods=['POST'])
 def editing():
-    name = request.form['name']
-    second_name = request.form['second_name']
-    patronymic = request.form['patronymic']
-    birth_place = request.form['birth_place']
-    birth_date = request.form['birth_date']
-    registration_adress = request.form['registration_adress']
-    basic_education = request.form['basic_education']
-    full_names_of_parents_work_place_phone_number = request.form['full_names_of_parents_work_place_phone_number']
-    financial_situation = request.form['financial_situation']
-    temporary_adress = request.form['temporary_adress']
-    phone_number = request.form['phone_number']
-    hobbies_and_interests = request.form['hobbies_and_interests']
-    work_place = request.form['work_place']
-    INN = request.form['INN']
-    passport_data = request.form['passport_data']
-    SNILS = request.form['SNILS']
+    if request.form['name']:
+        name = request.form['name']
+    if request.form['second_name']:
+        second_name = request.form['second_name']
+    if request.form['patronymic']:
+        patronymic = request.form['patronymic']
+    if request.form['birth_place']:
+        birth_place = request.form['birth_place']
+    if request.form['birth_date']:
+        birth_date = request.form['birth_date']
+    if request.form['registration_adress']:
+        registration_adress = request.form['registration_adress']
+    if request.form['basic_education']:
+        basic_education = request.form['basic_education']
+    if request.form['full_names_of_parents_work_place_phone_number']:
+        full_names_of_parents_work_place_phone_number = request.form['full_names_of_parents_work_place_phone_number']
+    if request.form['financial_situation']:
+        financial_situation = request.form['financial_situation']
+    if request.form['temporary_adress']:
+        temporary_adress = request.form['temporary_adress']
+    if request.form['phone_number']:
+        phone_number = request.form['phone_number']
+    if request.form['hobbies_and_interests']:
+        hobbies_and_interests = request.form['hobbies_and_interests']
+    if request.form['work_place']:
+        work_place = request.form['work_place']
+    if request.form['INN']:
+        INN = request.form['INN']
+    if request.form['passport_data']:
+        passport_data = request.form['passport_data']
+    if request.form['SNILS']:
+        SNILS = request.form['SNILS']
     id = request.form['id']
     student = Student.query.get(id)
-    student.name = name
-    student.second_name = second_name
-    student.patronymic = patronymic
-    student.birth_place = birth_place
-    student.birth_date = birth_date
-    student.registration_adress = registration_adress
-    student.basic_education = basic_education
-    student.full_names_of_parents_work_place_phone_number = full_names_of_parents_work_place_phone_number
-    student.financial_situation = financial_situation
-    student.temporary_adress = temporary_adress
-    student.phone_number =phone_number
-    student.hobbies_and_interests = hobbies_and_interests
-    student.work_place =work_place
-    student.INN = INN
-    student.passport_data = passport_data
-    student.SNILS = SNILS
+    if request.form['name']:
+        student.name = name
+    if request.form['second_name']:
+        student.second_name = second_name
+    if request.form['patronymic']:
+        student.patronymic = patronymic
+    if request.form['birth_place']:
+        student.birth_place = birth_place
+    if request.form['birth_date']:
+        student.birth_date = birth_date
+    if request.form['registration_adress']:
+        student.registration_adress = registration_adress
+    if request.form['basic_education']:
+        student.basic_education = basic_education
+    if request.form['full_names_of_parents_work_place_phone_number']:
+        student.full_names_of_parents_work_place_phone_number = full_names_of_parents_work_place_phone_number
+    if request.form['financial_situation']:
+        student.financial_situation = financial_situation
+    if request.form['temporary_adress']:
+        student.temporary_adress = temporary_adress
+    if request.form['phone_number']:
+        student.phone_number =phone_number
+    if request.form['hobbies_and_interests']:
+        student.hobbies_and_interests = hobbies_and_interests
+    if request.form['work_place']:
+        student.work_place =work_place
+    if request.form['name']:
+        student.INN = INN
+    if request.form['passport_data']:
+        student.passport_data = passport_data
+    if request.form['SNILS']:
+        student.SNILS = SNILS
     db.session.commit()
     return redirect(url_for('main'))
 
-@app.route('/search_results',methods=['POST'])
+@app.route('/search_results',methods=['GET'])
 def show_results():
     search_word = request.form['word']
     students = Student.query.whoosh_search(search_word).all()
